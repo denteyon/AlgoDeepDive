b4f9e4d6412e405f27e00e05f6e31004dd79d118
htetmyataung <htetmyataung2027@gmail.com>
2020-05-12 16:47:59 -0400

    add coderust

---
+++ CodeRust/Lcd.java
@@ -0,0 +1,24 @@
+package CodeRust;
+
+public class Lcd {
+    static int lcd(int a, int b){
+        if(a < b){
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        return (a*b)/gcd(a, b);
+    }
+    static int gcd(int a, int b){
+        if(b == 0){
+            return a;
+        } else {
+            return gcd(b, a%b);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lcd(10, 15));
+    }
+}
---
+++ CodeRust/NimarySearch.java
@@ -0,0 +1,20 @@
+package CodeRust;
+
+public class NimarySearch {
+    static int binarySearch(int[] arr, int k){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start+1 != end){
+            int mid = start + ((end - start) /2);
+            if(k == arr[mid]){
+                return mid;
+            } else if(k < arr[mid]){
+                end = mid;
+            } else if(k> arr[mid]){
+                start = mid;
+            }
+        }
+        return -1;
+    }
+}
