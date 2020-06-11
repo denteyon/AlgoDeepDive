d07372935b09ad9bcb25aa1e27dec060f2f5cc47
htetmyataung <htetmyataung2027@gmail.com>
2020-06-11 22:01:32 +0630

    update TOH

--- FamousAlgo/TowerOfHanoi.java
+++ FamousAlgo/TowerOfHanoi.java
@@ -1,11 +1,11 @@
 public class TowerOfHanoi {
     public static void solve(int n, String A, String B, String C) {
         if (n == 1) {
-            System.out.println(A + " -> " + C);
+            System.out.println(A + " -> " + C); //last disc
         } else {
-            solve(n - 1, A, C, B);
-            System.out.println(A + " -> " + C);
-            solve(n - 1, B, A, C);
+            solve(n - 1, A, C, B); // move n - 1 disc from A to C using B
+            System.out.println(A + " -> " + C); // move last disc
+            solve(n - 1, B, A, C); //move back n - 1 disc from B to C using A
         }
     }
 
